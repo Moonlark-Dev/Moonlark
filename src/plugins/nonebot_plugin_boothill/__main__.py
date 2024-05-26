c5ff08b082ebabca732a8abe6f302b3716f739c4
XiaoDeng3386 <1744793737@qq.com>
2024-05-26 19:30:59 +0800

    fix a ubg

--- src/plugins/nonebot_plugin_boothill/__main__.py
+++ src/plugins/nonebot_plugin_boothill/__main__.py
@@ -1,29 +1,24 @@
-from nonebot_plugin_alconna import Alconna, Args, Match, MultiVar, UniMessage, on_alconna
+from nonebot import on_command
+from nonebot_plugin_alconna import UniMessage
 from nonebot_plugin_htmlrender import md_to_pic
-
 from ..nonebot_plugin_larkutils.html import escape_html
-
 from ..nonebot_plugin_larkutils.user import get_user_id
-
 from ..nonebot_plugin_larklang.__main__ import LangHelper
-
 from .censor import censor
+from nonebot.adapters import Message
+from nonebot.params import CommandArg
 
-alc = Alconna(
-    "boothill",
-    Args["content", MultiVar(str)]
-)
-boothill = on_alconna(alc)
+boothill = on_command("boothill")
 lang = LangHelper()
 
 
 @boothill.handle()
-async def _(content: list[str], user_id: str = get_user_id()) -> None:
-    text = " ".join(content)
-    await boothill.finish(UniMessage().image(raw=await md_to_pic(
+async def _(user_id: str = get_user_id(), content: Message = CommandArg()) -> None:
+    text = content.extract_plain_text()
+    await boothill.finish(await UniMessage().image(raw=await md_to_pic(
         md=await lang.text(
             "cmd.md",
             user_id,
-            escape_html(censor(text))
+            escape_html(censor(text)).replace("\n", "<br>").replace("<br><br>", "\n\n")
         )
-    )), reply_message=True)
+    )).export(), reply_message=True)
