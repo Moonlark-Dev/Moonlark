db9722bf8228e9445ade2ef6b47250a314835919
XiaoDeng3386 <1744793737@qq.com>
2024-05-31 22:11:56 +0800

    使用 Enum 改写 LuckType

---
+++ src/plugins/nonebot_plugin_jrrp/types.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class LuckType(Enum):
+    PERFECT_LUCK = 1        # 100
+    ALMOST_PERFECT = 2      # 99
+    GREAT_DAY = 3           # 85-98
+    GOOD_DAY = 4            # 71-84
+    FAIR_DAY = 5            # 57-70
+    AVERAGE_DAY = 6         # 43-56
+    BELOW_AVERAGE = 7       # 29-42
+    POOR_DAY = 8            # 15-28
+    BAD_DAY = 9             # 1-14
+    TERRIBLE_LUCK = 10      # 0
--- src/plugins/nonebot_plugin_jrrp/utils.py
+++ src/plugins/nonebot_plugin_jrrp/utils.py
@@ -1,27 +1,28 @@
 from .lang import lang
 from .jrrp import get_luck_value
+from .types import LuckType
 
-def get_luck_type(luck_value: int) -> int:
+def get_luck_type(luck_value: int) -> LuckType:
     if luck_value == 100:
-        return 1
+        return LuckType.PERFECT_LUCK
     elif luck_value == 99:
-        return 2
+        return LuckType.ALMOST_PERFECT
     elif 99 > luck_value >= 85:
-        return 3
+        return LuckType.GREAT_DAY
     elif 85 > luck_value >= 71:
-        return 4
+        return LuckType.GOOD_DAY
     elif 71 > luck_value >= 57:
-        return 5
+        return LuckType.FAIR_DAY
     elif 57 > luck_value >= 43:
-        return 6
+        return LuckType.AVERAGE_DAY
     elif 43 > luck_value >= 29:
-        return 7
+        return LuckType.BELOW_AVERAGE
     elif 29 > luck_value >= 15:
-        return 8
+        return LuckType.POOR_DAY
     elif 15 > luck_value >= 1:
-        return 9
+        return LuckType.BAD_DAY
     else:
-        return 10
+        return LuckType.TERRIBLE_LUCK
 
 async def get_luck_message(user_id: str) -> str:
     luck_value = get_luck_value(user_id)
