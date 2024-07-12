af6f69692e52536066bc82c19361ccf96ba54f59
XiaoDeng3386 <1744793737@qq.com>
2024-07-12 22:19:26 +0800

    修复数据库含异常图片时无法投稿图片的问题

--- src/plugins/nonebot_plugin_larkcave/plugins/nonebot_plugin_cave_similarity_check/image.py
+++ src/plugins/nonebot_plugin_larkcave/plugins/nonebot_plugin_cave_similarity_check/image.py
@@ -3,6 +3,7 @@ import io
 from typing import AsyncGenerator
 from nonebot_plugin_orm import async_scoped_session
 from sqlalchemy import select
+from PIL import UnidentifiedImageError
 
 from .types import CheckResult
 from ...models import CaveData, ImageData
@@ -21,7 +22,11 @@ def compare_image(img1, img2):
 
 
 def compare_images(image1: bytes, image2: ImageData, *args) -> float:
-    return compare_image(np.array(Image.open(io.BytesIO(image1))), np.array(Image.open(io.BytesIO(image2.data))))
+    try:  # NOTE 解决数据库内空白图片的问题
+        img2 = np.array(Image.open(io.BytesIO(image2.data)))
+    except UnidentifiedImageError:
+        return 0
+    return compare_image(np.array(Image.open(io.BytesIO(image1))), img2)
 
 
 async def compare_images_async(image1: bytes, image2: ImageData, image1_name: str) -> float:
