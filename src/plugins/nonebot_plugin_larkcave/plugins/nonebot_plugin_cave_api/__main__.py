74e1f95678bc2d94ed841d5b2fe8d9a780c97b3e
XiaoDeng3386 <1744793737@qq.com>
2024-07-18 14:22:22 +0800

    更新 CaveAPI 实现
    支持了新的图片储存方法

--- src/plugins/nonebot_plugin_larkcave/plugins/nonebot_plugin_cave_api/__main__.py
+++ src/plugins/nonebot_plugin_larkcave/plugins/nonebot_plugin_cave_api/__main__.py
@@ -1,13 +1,35 @@
 import base64
 from nonebot import get_app
-from .types import RandomCaveResponse
+from nonebot_plugin_orm import get_session, get_scoped_session
+from sqlalchemy import select
+from typing import AsyncGenerator
+
+from .types import RandomCaveResponse, Image
 from ...__main__ import get_cave
-from nonebot_plugin_orm import get_session
 from ....nonebot_plugin_larkuser import get_user
 from ...models import ImageData
-from sqlalchemy import select
+from ...decoder import get_image
 
 app = get_app()
+
+
+async def get_image_data(cave_id: int) -> AsyncGenerator[Image, None]:
+    """
+    获取回声洞图片列表（生成器）
+    :param cave_id: 所属回声洞 ID
+    """
+    session = get_scoped_session()
+    for image in await session.scalars(select(ImageData).where(ImageData.belong == cave_id)):
+        try:
+            data = await get_image(image.id, session)
+        except Exception:
+            continue
+        yield {
+            "id": float(image.id),
+            "name": str(image.name),
+            "data": base64.b64encode(data.data).decode(),
+        }
+    await session.close()
 
 
 @app.get("/api/cave/random")
@@ -19,8 +41,5 @@ async def _() -> RandomCaveResponse:
             "content": str(cave.content),
             "time": cave.time.timestamp(),
             "author": (await get_user(cave.author)).get_nickname(),
-            "images": [
-                {"id": float(image.id), "name": str(image.name), "data": base64.b64encode(image.data).decode()}
-                for image in (await session.scalars(select(ImageData).where(ImageData.belong == cave.id))).all()
-            ],
+            "images": [img async for img in get_image_data(cave.id)]
         }
