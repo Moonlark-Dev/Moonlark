0e9b36ba735fe9204dead987e7948007472c8a9e
XiaoDeng3386 <1744793737@qq.com>
2024-08-06 14:31:55 +0800

    更新了 nonebot_plugin_render 和 nonebot_plugin_sign 插件的代码实现

--- src/plugins/nonebot_plugin_render/theme.py
+++ src/plugins/nonebot_plugin_render/theme.py
@@ -1,10 +1,10 @@
 import json
 from pathlib import Path
 import aiofiles
-from nonebot_plugin_orm import get_session
-
-from .models import ThemeConfig
+from nonebot_plugin_localstore import get_data_dir
 from .config import config
+
+data_dir = get_data_dir("nonebot_plugin_render")
 
 
 async def get_themes() -> dict[str, str]:
@@ -13,22 +13,18 @@ async def get_themes() -> dict[str, str]:
 
 
 async def get_user_theme(user_id: str) -> str:
-    async with get_session() as session:
-        user = await session.get(ThemeConfig, user_id)
-        theme = user.theme if user else config.render_default_theme
-    if theme not in await get_themes():
+    file = data_dir.joinpath(user_id)
+    if file.exists():
+        async with aiofiles.open(file, "r", encoding="UTF-8") as f:
+            theme = await f.read()
+    if theme not in await get_themes() or not file.exists():
         theme = config.render_default_theme
     return theme
 
 
 async def set_theme(user_id: str, theme: str) -> None:
-    async with get_session() as session:
-        user = await session.get(ThemeConfig, user_id)
-        if user:
-            user.theme = theme
-        else:
-            session.add(ThemeConfig(user_id=user_id, theme=theme))
-        await session.commit()
+    async with open(data_dir.joinpath(user_id), "w", encoding="utf-8") as f:
+        await f.write(theme)
 
 
 async def get_theme_file(theme: str) -> str:
