948d9fde8868cc5ca3af4f291d964610cfdcb98f
xxtg666 <xxtg666@gmail.com>
2024-08-08 20:47:04 +0800

    添加 FTT 图片渲染

--- src/plugins/nonebot_plugin_finding_the_trail/utils/image/generator.py
+++ src/plugins/nonebot_plugin_finding_the_trail/utils/image/generator.py
@@ -15,15 +15,35 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # ##############################################################################
 
-from typing import TYPE_CHECKING
 from PIL import Image
-if TYPE_CHECKING:
-    from ..fttmap import Blocks
+from pathlib import Path
+from ..fttmap import Blocks
 
+base_path = Path(__file__).parent.joinpath("assets")
+
+BLOCKS = {
+    Blocks.NULL: Image.open(base_path.joinpath("stone_bricks.png")),
+    Blocks.WALL: Image.open(base_path.joinpath("bricks.png")),
+    Blocks.START: Image.open(base_path.joinpath("iron_block.png")),
+    Blocks.END: Image.open(base_path.joinpath("diamond_block.png")),
+    Blocks.PISTON: Image.open(base_path.joinpath("piston_top.png")),
+    Blocks.SAND: Image.open(base_path.joinpath("sand.png")),
+    Blocks.COBWEB: Image.open(base_path.joinpath("cobweb.png")),
+    Blocks.PORTAL: Image.open(base_path.joinpath("portal.png")),
+}
 
 def generate_map_image(game_map: "list[list[Blocks]]") -> Image:
     """
     生成地图图片
     :param game_map: 游戏地图
     """
-    pass
+    image = Image.new(
+        "RGB", (len(game_map[0]) * 16, len(game_map) * 16), (51, 255, 255)
+    )
+    for row in range(len(game_map)):
+        for column in range(len(game_map[row])):
+            item = game_map[row][column]
+            x0 = column * 16
+            y0 = row * 16
+            image.paste(BLOCKS[item], (x0, y0))
+    return image
