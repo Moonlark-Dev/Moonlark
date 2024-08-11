b8d92bc85c01477d4b92715d613e27b072efd0d0
XiaoDeng3386 <1744793737@qq.com>
2024-08-11 12:39:17 +0800

    fix a bug

--- src/plugins/nonebot_plugin_bag/utils/give.py
+++ src/plugins/nonebot_plugin_bag/utils/give.py
@@ -1,10 +1,11 @@
 from typing import Literal
+from nonebot.log import logger
 from ...nonebot_plugin_item.types import DictItemData
 from ...nonebot_plugin_item.utils.string import get_location_by_id
 from ...nonebot_plugin_item.utils.get import get_item
 from ...nonebot_plugin_larkuser import get_user
 from ..types import GivenItemsData
-from .bag import give_item
+from .bag import give_item, give_special_item
 
 
 async def give_item_by_data(user_id: str, items: GivenItemsData) -> None:
@@ -17,6 +18,7 @@ async def give_item_by_data(user_id: str, items: GivenItemsData) -> None:
         await give_item(user_id, item)
 
 
+"""
 async def give_special_item(user_id: str, name: str, count: int) -> None:
     user = await get_user(user_id)
     match name:
@@ -26,13 +28,16 @@ async def give_special_item(user_id: str, name: str, count: int) -> None:
             await user.add_vimcoin(count)
         case _:
             raise ValueError(f"{name} is not a valid special item name")
-
+"""
 
 async def give_item_by_list(user_id: str, items: list[DictItemData]) -> None:
     for item_data in items:
+        logger.debug(f"Current item: {item_data}")
         location = get_location_by_id(item_data["item_id"])
         if location.getNamespace() == "special":
             await give_special_item(user_id, location.getPath(), item_data["count"])
             continue
+        logger.debug(f"Location got: {location}")
         item = await get_item(location, user_id, item_data["count"], item_data["data"])
+        logger.debug(f"Item got: {item}")
         await give_item(user_id, item)
