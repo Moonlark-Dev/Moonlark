7b3827e4e0dd5d29b158f34b30df948e4cb276d6
XiaoDeng3386 <1744793737@qq.com>
2024-08-26 21:37:29 +0800

    新增帮助获取接口

--- src/plugins/nonebot_plugin_larkhelp/__main__.py
+++ src/plugins/nonebot_plugin_larkhelp/__main__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Any
+from typing import Any, Optional
 
 from nonebot import get_driver
 from nonebot_plugin_alconna import Alconna, Args, on_alconna
@@ -10,8 +10,10 @@ from ..nonebot_plugin_render.cache import creator
 
 from ..nonebot_plugin_larklang.__main__ import LangHelper
 from ..nonebot_plugin_larkutils import get_user_id
+from .models import CommandHelp
 from .collector import collect_command_help
 
+help_list = {}
 
 @get_driver().on_startup
 async def _() -> None:
@@ -22,6 +24,8 @@ async def _() -> None:
 help_cmd = on_alconna(Alconna("help", Args["command?", str]))
 lang = LangHelper()
 
+def get_help_list() -> dict[str, CommandHelp]:
+    return help_list
 
 @help_cmd.assign("command")
 async def _(command: str, user_id: str = get_user_id()) -> None:
@@ -41,19 +45,25 @@ async def _(command: str, user_id: str = get_user_id()) -> None:
     await help_cmd.finish()
 
 
+async def get_help_dict(name: str, user_id: str, data: Optional[CommandHelp] = None) -> dict[str, str | list[str]]:
+    data = data or help_list[name]
+    return {
+        "name": name,
+        "description": await (plugin_lang := LangHelper(data.plugin)).text(data.description, user_id),
+        "details": await plugin_lang.text(data.details, user_id),
+        "usages": [
+            (await lang.text("list.usage", user_id, await plugin_lang.text(usage, user_id)))
+            for usage in data.usages
+        ],
+    }
+
 async def get_templates(user_id: str) -> dict[str, Any]:
+    if not help_list:
+        raise ValueError("No Command")
     return dict(
         usages_text=await lang.text("list.usage_text", user_id),
         commands=[
-            {
-                "name": name,
-                "description": await (plugin_lang := LangHelper(data.plugin)).text(data.description, user_id),
-                "details": await plugin_lang.text(data.details, user_id),
-                "usages": [
-                    (await lang.text("list.usage", user_id, await plugin_lang.text(usage, user_id)))
-                    for usage in data.usages
-                ],
-            }
+            await get_help_dict(name, user_id, data)
             for name, data in help_list.items()
         ],
     )
@@ -62,7 +72,11 @@ async def get_templates(user_id: str) -> dict[str, Any]:
 @creator("help.html.jinja")
 async def render(user_id: str) -> bytes:
     return await render_template(
-        "help.html.jinja", await lang.text("list.title", user_id), user_id, await get_templates(user_id), True
+        "help.html.jinja",
+        await lang.text("list.title", user_id),
+        user_id,
+        await get_templates(user_id),
+        True
     )
 
 
---
+++ src/plugins/nonebot_plugin_larkhelp/api.py
@@ -0,0 +1,40 @@
+#  Moonlark - A new ChatBot
+#  Copyright (C) 2024  Moonlark Development Team
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ##############################################################################
+
+from nonebot import get_app
+from typing import cast
+from fastapi import FastAPI, HTTPException, status
+
+from ..nonebot_plugin_larkuid.session import get_user_id
+from .__main__ import lang, get_help_list, get_help_dict
+
+app = cast(FastAPI, get_app())
+
+
+@app.get("/api/help/commands")
+async def _() -> list[str]:
+    return list(get_help_list().keys())
+
+
+@app.get("/api/help/commands/{name}")
+async def _(name: str, user_id: str = get_user_id("-1")) -> dict[str, str | list[str]]:
+    try:
+        return await get_help_dict(name, user_id)
+    except KeyError:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+
