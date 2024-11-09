21c9e8796b05fe8f079d0197545d9893bf0d3824
XiaoDeng3386 <1744793737@qq.com>
2024-11-09 15:37:13 +0800

    添加一个 config

--- src/plugins/nonebot_plugin_larkuser/matchers/recorder.py
+++ src/plugins/nonebot_plugin_larkuser/matchers/recorder.py
@@ -1,4 +1,5 @@
 from ..utils.avatar import is_user_avatar_updated, update_user_avatar
+from ..utils.user import get_user
 from ...nonebot_plugin_larkutils import get_main_account
 from nonebot import on_message
 from nonebot.log import logger
@@ -20,6 +21,8 @@ async def _(session: async_scoped_session, user: UserInfo = EventUserInfo()) -> 
         user_data = UserData(user_id=user.user_id, nickname=user.user_name)
         session.add(user_data)
         return await session.commit()
+    if (await get_user(user.user_id)).get_config_key("lock_nickname_and_avatar", False):
+        return
     if user_data.nickname != user.user_name:
         logger.info(f"用户 {user_data.user_id} 修改了其昵称 ({user_data.nickname} => {user.user_name})")
         user_data.nickname = user.user_name
