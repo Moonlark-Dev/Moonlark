8615e344a065502c849dde305b2189f1e9a09060
XiaoDeng3386 <1744793737@qq.com>
2024-11-15 22:30:08 +0800

    Opt: Lang 的数据库缓存和显示设置数据表

--- src/plugins/nonebot_plugin_render/theme.py
+++ src/plugins/nonebot_plugin_render/theme.py
@@ -1,11 +1,11 @@
+from nonebot_plugin_orm import get_session
 import json
 from pathlib import Path
 import aiofiles
 from nonebot_plugin_localstore import get_data_dir
 from .config import config
 from ..nonebot_plugin_larkutils import parse_special_user_id
-
-data_dir = get_data_dir("nonebot_plugin_render")
+from ..nonebot_plugin_larklang.models import DisplaySetting
 
 
 async def get_themes() -> dict[str, str]:
@@ -16,20 +16,27 @@ async def get_themes() -> dict[str, str]:
 async def get_user_theme(user_id: str) -> str:
     if user_id.startswith("mlsid::") and "--theme" in (args := parse_special_user_id(user_id)):
         return args["--theme"]
-    file = data_dir.joinpath(user_id)
-    if file.exists():
-        async with aiofiles.open(file, "r", encoding="UTF-8") as f:
-            theme = await f.read()
-        if theme not in await get_themes():
-            theme = config.render_default_theme
-    else:
+    async with get_session() as session:
+        user = await session.get(DisplaySetting, user_id)
+        if user is None:
+            return config.render_default_theme
+        theme = user.theme
+    if theme not in await get_themes():
         theme = config.render_default_theme
     return theme
 
-
 async def set_theme(user_id: str, theme: str) -> None:
-    async with aiofiles.open(data_dir.joinpath(user_id), "w", encoding="utf-8") as f:
-        await f.write(theme)
+    async with get_session() as session:
+        user = await session.get(DisplaySetting, user_id)
+        if user is None:
+            user = DisplaySetting(
+                user_id=user_id,
+                theme=theme
+            )
+        else:
+            user.theme=theme
+        await session.merge(user)
+        await session.commit()
 
 
 async def get_theme_file(theme: str) -> str:
