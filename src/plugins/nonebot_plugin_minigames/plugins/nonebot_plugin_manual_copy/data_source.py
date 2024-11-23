d1f96aa2599d349b1d2fe96c986a905fc1050514
XiaoDeng3386 <1744793737@qq.com>
2024-11-23 22:15:53 +0800

    添加打字机小游戏 #224

---
+++ src/plugins/nonebot_plugin_minigames/plugins/nonebot_plugin_manual_copy/data_source.py
@@ -0,0 +1,45 @@
+#  Moonlark - A new ChatBot
+#  Copyright (C) 2024  Moonlark Development Team
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ##############################################################################
+
+import httpx
+from .config import config
+from typing import TypedDict, Optional
+import asyncio
+
+class QuestionData(TypedDict):
+    origin: str
+    text: str
+    length: int
+
+async def request_question() -> Optional[QuestionData]:
+    async with httpx.AsyncClient() as client:
+        request = await client.get(config.manual_copy_api)
+    if request.status_code == 200:
+        data = request.json()
+        return {
+            "origin": data["from"],
+            "text": data["hitokoto"],
+            "length": data["length"]
+        }
+
+async def get_question() -> QuestionData:
+    while (question := await request_question()) is not None:
+        await asyncio.sleep(1)
+    return question
+
+
+
---
+++ src/plugins/nonebot_plugin_minigames/plugins/nonebot_plugin_manual_copy/main.py
@@ -0,0 +1,42 @@
+#  Moonlark - A new ChatBot
+#  Copyright (C) 2024  Moonlark Development Team
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ##############################################################################
+
+from nonebot_plugin_alconna import on_alconna, Alconna, UniMessage
+from ..nonebot_plugin_minigames_api import create_minigame_session
+from ....nonebot_plugin_larkuser.utils.waiter import prompt
+from ....nonebot_plugin_larkutils import get_user_id
+from ....nonebot_plugin_larklang import LangHelper
+from nonebot_plugin_htmlrender import md_to_pic
+from .data_source import get_question
+
+matcher = on_alconna(Alconna("manual-copy"))
+lang = LangHelper()
+
+
+@matcher.handle()
+async def _(user_id: str = get_user_id()) -> None:
+    question = await get_question()
+    raw_image = await md_to_pic(await lang.text("question", user_id, question["text"], question["origin"]))
+    image = UniMessage().image(raw=raw_image)
+    session = await create_minigame_session(user_id)
+    await prompt(image, user_id, lambda string: string.strip() == question["text"])
+    t = await session.finish()
+    points = await session.add_points(round(question["length"] / t * 100))
+    await lang.finish("finish", user_id, round(t, 3), points)
+
+
+
