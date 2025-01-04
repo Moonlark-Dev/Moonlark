b369d6d7ae8cdce4f1e9467e102c2ed9156ce96e
XiaoDeng3386 <1744793737@qq.com>
2025-01-04 16:22:40 +0800

    添加 /apt/bots 接口

--- src/plugins/nonebot_plugin_bots/__main__.py
+++ src/plugins/nonebot_plugin_bots/__main__.py
@@ -1,14 +1,53 @@
 import time
+from nonebot.adapters.onebot.v11.bot import Bot as V11Bot
+from nonebot.adapters.onebot.v12.bot import Bot as V12Bot
+from nonebot.adapters.qq import Bot as QQBot
 from nonebot_plugin_apscheduler import scheduler
 from nonebot import logger
 from nonebot.message import event_preprocessor
 from nonebot.adapters import Bot, Event
-from nonebot.exception import IgnoredException
+from nonebot.exception import IgnoredException, ActionFailed
+from nonebot import get_bot
+from nonebot import get_app
+from fastapi import FastAPI, Request
+from typing import cast
 
 from .config import config
-
+from .types import BotStatus
 
 sessions: dict[str, tuple[str, float]] = {}
+
+
+@cast(FastAPI, get_app()).get("/api/bots")
+async def bots_status(_: Request) -> dict[str, BotStatus]:
+    bots: dict[str, BotStatus] = {}
+    for code, user_id in config.bots_list.items():
+        try:
+            bot = get_bot(user_id)
+        except KeyError:
+            bots[code] = {
+                "user_id": user_id,
+                "online": False
+            }
+            continue
+        try:
+            if isinstance(bot, QQBot):
+                good = await bot.ready()
+            elif isinstance(bot, V11Bot):
+                good = (await bot.get_status())["good"]
+            elif isinstance(bot, V12Bot):
+                good = (await bot.get_status())["good"]
+            else:
+                good = False
+        except ActionFailed:
+            good = False
+        bots[code] = {
+            "user_id": user_id,
+            "adapter_name": bot.adapter.get_name(),
+            "online": True,
+            "good": good
+        }
+    return bots
 
 
 @event_preprocessor
@@ -35,3 +74,4 @@ async def _() -> None:
             logger.debug(f"将回收过期会话: {key} ({value})")
     for key in expired_sessions:
         sessions.pop(key)
+
