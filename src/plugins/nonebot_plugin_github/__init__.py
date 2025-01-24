e4089b56767102aa5db1278c172a5c0bb3665966
xxtg666 <xxtg666@gmail.com>
2025-01-24 13:09:28 +0800

    初步实现 github 链接解析

---
+++ src/plugins/nonebot_plugin_github/__init__.py
@@ -0,0 +1,16 @@
+from nonebot import require
+from nonebot.plugin import PluginMetadata
+
+
+__plugin_meta__ = PluginMetadata(
+    name="nonebot_plugin_github",
+    description="",
+    usage="",
+)
+
+
+require("nonebot_plugin_render")
+require("nonebot_plugin_larklang")
+require("nonebot_plugin_larkutils")
+
+from . import __main__
