fda756da3478556e534865f7e701012d5a4abb58
XiaoDeng3386 <1744793737@qq.com>
2025-02-06 15:43:06 +0800

    feat: Cave 投稿者统计

---
+++ src/plugins/nonebot_plugin_larkcave/plugins/statisics/__init__.py
@@ -0,0 +1,33 @@
+from nonebot import require
+from nonebot.plugin import PluginMetadata
+
+
+__plugin_meta__ = PluginMetadata(
+    name="nonebot_plugin_cave_statisics",
+    description="",
+    usage="",
+    config=None,
+)
+
+require("nonebot_plugin_larkuser")
+require("nonebot_plugin_alconna")
+require("nonebot_plugin_larkutils")
+require("nonebot_plugin_orm")
+
+from nonebot_plugin_larkutils.user import get_user_id
+from nonebot_plugin_orm import async_scoped_session
+from nonebot_plugin_alconna import UniMessage
+from ...lang import lang
+from ...__main__ import cave
+from .data import merge_small_poster, set_nickname_for_posters, get_poster_data
+
+
+@cave.assign("statisics")
+async def _(session: async_scoped_session, user_id: str = get_user_id()) -> None:
+    await lang.send("stat.tip", user_id)
+    data = await merge_small_poster(await set_nickname_for_posters(await get_poster_data(session)), user_id)
+    # TODO 在这里绘图，或者建个 image.py 调函数，然后把下面 None 换掉
+    await cave.finish(UniMessage().image(raw=None))
+
+
+
---
+++ src/plugins/nonebot_plugin_larkcave/plugins/statisics/data.py
@@ -0,0 +1,40 @@
+from sqlalchemy import select
+from ...models import CaveData
+from nonebot_plugin_orm import async_scoped_session
+from nonebot_plugin_larkuser import get_user
+from ...lang import lang
+
+
+
+async def get_poster_data(session: async_scoped_session) -> dict[str, int]:
+    posters = {}
+    for poster in await session.scalars(select(CaveData.author).where(CaveData.public == True)):
+        if poster in posters:
+            posters[poster] += 1
+        else:
+            posters[poster] = 1
+    return posters
+    
+
+async def set_nickname_for_posters(data: dict[str, int]) -> dict[str, int]:
+    posters = {}
+    for user_id, count in data.items():
+        nickname = (await get_user(user_id)).get_nickname()
+        if user_id == -1 and nickname in posters:
+            posters[nickname] += count
+        posters[nickname] = count
+    return posters
+
+async def merge_small_poster(data: dict[str, int], sender_id: str) -> dict[str, int]:
+    posters = {}
+    lowest = max(sum([i for i in data.values()]) / len(data) * 0.05, 3)
+    other_key_name = await lang.text("stat.other", sender_id)
+    for key, count in data.items():
+        if count < lowest and other_key_name not in posters:
+            posters[other_key_name] += count
+        else:
+            posters[other_key_name] = count
+        posters[key] = count
+    return posters
+
+
