52d249bee3b015883e63bcf34a0bc02a61c28df2
XiaoDeng3386 <1744793737@qq.com>
2025-02-09 11:30:27 +0800

    fix a bug

--- src/plugins/nonebot_plugin_larkcave/plugins/statisics/data.py
+++ src/plugins/nonebot_plugin_larkcave/plugins/statisics/data.py
@@ -2,6 +2,7 @@ from sqlalchemy import select
 from ...models import CaveData
 from nonebot_plugin_orm import async_scoped_session
 from nonebot_plugin_larkuser import get_user
+from nonebot_plugin_larkuser.user import MoonlarkGuestUser
 from ...lang import lang
 
 
@@ -15,13 +16,16 @@ async def get_poster_data(session: async_scoped_session) -> dict[str, int]:
     return posters
 
 
-async def set_nickname_for_posters(data: dict[str, int]) -> dict[str, int]:
+async def set_nickname_for_posters(data: dict[str, int], sender_id: str) -> dict[str, int]:
+    other_key_name = await lang.text("stat.other", sender_id)
     posters = {}
     for user_id, count in data.items():
-        nickname = (await get_user(user_id)).get_nickname()
-        if user_id == -1 and nickname in posters:
-            posters[nickname] += count
-        posters[nickname] = count
+        user = await get_user(user_id)
+        if not user.has_nickname():
+            posters[other_key_name] = count + posters.get(other_key_name, 0)
+            continue
+        nickname = user.get_nickname()
+        posters[nickname] = count + posters.get(nickname, 0)
     return posters
 
 
@@ -30,9 +34,10 @@ async def merge_small_poster(data: dict[str, int], sender_id: str) -> dict[str, 
     lowest = max(sum([i for i in data.values()]) / len(data) * 0.05, 3)
     other_key_name = await lang.text("stat.other", sender_id)
     for key, count in data.items():
-        if count < lowest and other_key_name not in posters:
-            posters[other_key_name] += count
+        if key == other_key_name:
+            continue
+        elif count < lowest:
+            posters[other_key_name] = count + posters.get(other_key_name, 0)
         else:
-            posters[other_key_name] = count
-        posters[key] = count
+            posters[key] = count
     return posters
