a704a9e5f7c613d6510efd7d80f60231df1c22cd
XiaoDeng3386 <1744793737@qq.com>
2025-07-11 13:44:23 +0800

    添加聊天记录总结插件

---
+++ src/plugins/nonebot_plugin_message_summary/__main__.py
@@ -0,0 +1,111 @@
+import aiofiles
+from nonebot_plugin_htmlrender import md_to_pic
+from nonebot_plugin_larkuser import get_user
+from sqlalchemy import select
+from nonebot.adapters import Event, Bot
+from nonebot.adapters.qq import Bot as Bot_QQ
+from nonebot.adapters.onebot.v11 import GroupMessageEvent
+from nonebot import on_message
+import json
+from nonebot_plugin_orm import async_scoped_session
+from nonebot_plugin_alconna import on_alconna, Alconna, Subcommand, Args, UniMessage
+from nonebot_plugin_openai import fetch_messages, generate_message
+from nonebot_plugin_larkutils import get_user_id, get_group_id
+from nonebot_plugin_larklang import LangHelper
+from nonebot_plugin_localstore import get_cache_file
+
+from .models import GroupMessage
+
+lang = LangHelper()
+summary = on_alconna(Alconna(
+    "summary",
+    Subcommand("--enable|-e"),
+    Subcommand("--disable|-d"),
+    Args["limit", int, 200]
+))
+config_file = get_cache_file("nonebot-plugin-message-summary", "config.json")
+recorder = on_message(priority=3, block=False)
+
+
+async def get_config() -> list[str]:
+    if not config_file.is_file():
+        return []
+    async with aiofiles.open(config_file, "r") as f:
+        return json.loads(await f.read())
+
+
+@summary.assign("$main")
+async def _(limit: int, session: async_scoped_session, user_id: str = get_user_id(), group_id: str = get_group_id()) -> None:
+    if group_id not in await get_config():
+        await lang.finish("disabled", user_id)
+    result = (await session.scalars(select(GroupMessage).where(GroupMessage.group_id == group_id).order_by(GroupMessage.id_.desc()).limit(limit).order_by(GroupMessage.id_))).all()
+    messages = ""
+    for message in result:
+        messages += f"[{message.sender_nickname}] {message.message}\n"
+    summary_string = await fetch_messages(
+        [
+            generate_message(await lang.text("prompt", user_id), "system"),
+            generate_message(messages, "user")
+        ],
+        user_id
+    )
+    await summary.finish(UniMessage().image(raw=await md_to_pic(summary_string)))
+
+
+async def clean_recorded_message(session: async_scoped_session, group_id: str) -> None:
+    result = (await session.scalars(
+        select(GroupMessage).where(GroupMessage.group_id == group_id).order_by(GroupMessage.id_))).all()
+    if len(result) > 270:
+        for i in range(270 - len(result)):
+            await session.delete(result[i])
+
+@recorder.handle()
+async def _(event: GroupMessageEvent, session: async_scoped_session, group_id: str = get_group_id()) -> None:
+    if group_id not in await get_config():
+        await recorder.finish()
+    await clean_recorded_message(session, group_id)
+    session.add(GroupMessage(
+        message=event.raw_message,
+        sender_nickname=event.sender.nickname,
+        group_id=group_id
+    ))
+    await session.commit()
+    await recorder.finish()
+
+
+@recorder.handle()
+async def _(event: Event, session: async_scoped_session, group_id: str = get_group_id(), user_id: str = get_user_id()) -> None:
+    if group_id not in await get_config():
+        await recorder.finish()
+    await clean_recorded_message(session, group_id)
+    session.add(GroupMessage(
+        message=event.get_plaintext(),
+        sender_nickname=(await get_user(user_id)).get_nickname(),
+        group_id=group_id
+    ))
+    await session.commit()
+
+@summary.assign("enable")
+async def _(bot: Bot, user_id: str = get_user_id(), group_id: str = get_group_id()) -> None:
+    if isinstance(bot, Bot_QQ):
+        await lang.finish("switch.unsupported", user_id)
+    config = await get_config()
+    if group_id not in config:
+        config.append(group_id)
+    async with aiofiles.open(config_file, "w") as f:
+        await f.write(json.dumps(config))
+    await lang.finish("switch.enable", user_id)
+
+@summary.assign("disable")
+async def _(user_id: str = get_user_id(), group_id: str = get_group_id()) -> None:
+    config = await get_config()
+    if group_id in config:
+        config.pop(config.index(group_id))
+    async with aiofiles.open(config_file, "w") as f:
+        await f.write(json.dumps(config))
+    await lang.finish("switch.disable", user_id)
+
+
+
+
+
---
+++ src/plugins/nonebot_plugin_message_summary/models.py
@@ -0,0 +1,13 @@
+
+from nonebot_plugin_orm import Model
+from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy import String, Text
+
+
+class GroupMessage(Model):
+    id_: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    message: Mapped[str] = mapped_column(Text())
+    sender_nickname: Mapped[str] = mapped_column(String(128))
+    group_id: Mapped[str] = mapped_column(String(128))
+
+
