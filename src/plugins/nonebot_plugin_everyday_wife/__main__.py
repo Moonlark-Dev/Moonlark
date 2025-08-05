ed2062062213fe03158f3735114ec6d23d844939
XiaoDeng3386 <1744793737@qq.com>
2025-08-05 23:18:16 +0800

    新增两个子命令

--- src/plugins/nonebot_plugin_everyday_wife/__main__.py
+++ src/plugins/nonebot_plugin_everyday_wife/__main__.py
@@ -27,14 +27,14 @@ from sqlalchemy.orm import mapped_column, Mapped
 from nonebot.adapters.onebot.v11 import Bot as OneBotV11Bot
 from nonebot.adapters.onebot.v12 import Bot as OneBotV12Bot
 from nonebot.adapters.qq import Bot as QQBot
-from typing import cast, Optional
+from typing import cast, Optional, NoReturn
 from nonebot.matcher import Matcher
-from nonebot.adapters import Event, Bot
+from nonebot.adapters import Event, Bot, Message
+from nonebot.params import CommandArg
 from nonebot_plugin_session import SessionId, SessionIdType
 from nonebot_plugin_larkutils import get_user_id, get_group_id
 
 lang = LangHelper()
-
 
 class WifeData(Model):
     id_: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
@@ -44,6 +44,23 @@ class WifeData(Model):
     queried: Mapped[bool] = mapped_column(default=False)
     generate_date: Mapped[date]
 
+async def marry(couple: tuple[str, str], group_id: str) -> None:
+    today = date.today()
+    async with get_session() as session:
+        await session.merge(WifeData(
+            group_id=group_id,
+            user_id=couple[0],
+            wife_id=couple[1],
+            generate_date=today,
+            queried=False
+        ))
+        await session.merge(WifeData(
+            group_id=group_id,
+            user_id=couple[1],
+            wife_id=couple[0],
+            generate_date=today,
+            queried=False
+        ))
 
 async def init_group(members: list[str], group_id: str) -> None:
     unmatched_members = []
@@ -51,28 +68,19 @@ async def init_group(members: list[str], group_id: str) -> None:
     async with get_session() as session:
         for member in members:
             user_id = str(member)
-            result = cast(
-                Optional[WifeData],
-                await session.scalar(
-                    select(WifeData).where(WifeData.group_id == group_id, WifeData.user_id == user_id)
-                ),
-            )
+            result = cast(Optional[WifeData], await session.scalar(select(WifeData).where(
+                WifeData.group_id == group_id,
+                WifeData.user_id == user_id
+            )))
             if result is None or result.generate_date != today:
                 unmatched_members.append(user_id)
-        c = len(unmatched_members) // 2
-        for i in range(c):
-            couple = (
-                unmatched_members.pop(random.randint(0, len(unmatched_members) - 1)),
-                unmatched_members.pop(random.randint(0, len(unmatched_members) - 1)),
-            )
-            await session.merge(
-                WifeData(group_id=group_id, user_id=couple[0], wife_id=couple[1], generate_date=today, queried=False)
-            )
-            await session.merge(
-                WifeData(group_id=group_id, user_id=couple[1], wife_id=couple[0], generate_date=today, queried=False)
-            )
-        await session.commit()
-
+    c = len(unmatched_members) // 2
+    for i in range(c):
+        couple = (
+            unmatched_members.pop(random.randint(0, len(unmatched_members) - 1)),
+            unmatched_members.pop(random.randint(0, len(unmatched_members) - 1)),
+        )
+        await marry(couple, group_id)
 
 async def init_onebot_v11_group(bot: OneBotV11Bot, group_id: str) -> None:
     members = await bot.get_group_member_list(group_id=int(group_id))
@@ -83,26 +91,49 @@ async def init_onebot_v12_group(bot: OneBotV12Bot, group_id: str) -> None:
     members = await bot.get_group_member_list(group_id=group_id)
     await init_group([user["user_id"] for user in members], group_id)
 
-
 async def init_qq_group(bot: QQBot, group_id: str) -> None:
     members = await bot.post_group_members(group_id=group_id)
     await init_group([user.member_openid for user in members.members], group_id)
 
-
 from nonebot_plugin_userinfo import get_user_info
 
+async def divorce(group_id: str, session: async_scoped_session, platform_user_id: str) -> None:
+    query = cast(Optional[WifeData], await session.scalar(select(WifeData).where(
+        WifeData.user_id == platform_user_id,
+        WifeData.group_id == group_id
+    )))
+    if query:
+        result = await session.scalar(select(WifeData).where(
+            WifeData.user_id == query.wife_id,
+            WifeData.group_id == group_id
+        ))
+        if result:
+            await session.delete(result)
+        await session.delete(query)
+    await session.commit()
+
+def get_at_argument(message: Message) -> Optional[str]:
+    for seg in message:
+        if seg.type == "at":
+            return seg.data["user_id"]
+    return None
 
 @on_command("wife", aliases={"today-wife", "waifu"}).handle()
 async def _(
-    matcher: Matcher,
-    event: Event,
-    session: async_scoped_session,
-    bot: Bot,
-    user_id: str = get_user_id(),
-    group_id: str = SessionId(
-        SessionIdType.GROUP, include_bot_type=False, include_bot_id=False, include_platform=False
-    ),
+        matcher: Matcher,
+        event: Event,
+        session: async_scoped_session,
+        bot: Bot,
+        user_id: str = get_user_id(),
+        group_id: str = SessionId(
+            SessionIdType.GROUP,
+            include_bot_type=False,
+            include_bot_id=False,
+            include_platform=False
+        ),
+        arg_message: Message = CommandArg()
 ) -> None:
+    argv = arg_message.extract_plain_text().strip().lower()
     if isinstance(bot, OneBotV11Bot):
         await init_onebot_v11_group(bot, group_id)
     elif isinstance(bot, OneBotV12Bot):
@@ -110,22 +141,39 @@ async def _(
     elif isinstance(bot, QQBot):
         await init_qq_group(bot, group_id)
     platform_user_id = event.get_user_id()
-    query = cast(
-        Optional[WifeData],
-        await session.scalar(
-            select(WifeData).where(
-                WifeData.user_id == platform_user_id,
-                WifeData.group_id == group_id,
-                WifeData.generate_date == date.today(),
-            )
-        ),
-    )
+    if argv == "divorce":
+        await divorce(group_id, session, platform_user_id)
+        await lang.finish("divorce", user_id, at_sender=True, reply_message=True)
+    elif argv.startswith("force-marry"):
+        target = get_at_argument(arg_message)
+        if target is None:
+            await lang.finish("no_target", user_id, at_sender=True, reply_message=True)
+        await divorce(group_id, session, platform_user_id)
+        await divorce(group_id, session, platform_user_id)
+        await marry((platform_user_id, target), group_id)
+        await lang.finish("force_success", user_id, at_sender=True, reply_message=True)
+
+    query = cast(Optional[WifeData], await session.scalar(select(WifeData).where(
+        WifeData.user_id == platform_user_id,
+        WifeData.group_id == group_id,
+        WifeData.generate_date == date.today()
+    )))
     if query is None:
         await lang.finish("unmatched", user_id, at_sender=True)
-    query.queried = True
     user_info = await get_user_info(bot, event, query.wife_id)
     message = UniMessage().text(text=await lang.text("matched", user_id)).at(user_id=query.wife_id)
-    await session.commit()
     if user_info.user_avatar:
         message = message.image(url=user_info.user_avatar.get_url())
+    if query.queried:
+        message = message.text(text=await lang.text("queried", user_id))
+    query.queried = True
+    await session.commit()
     await matcher.finish(await message.export(), reply_message=True)
+
+
+
+
+
+
+
+
