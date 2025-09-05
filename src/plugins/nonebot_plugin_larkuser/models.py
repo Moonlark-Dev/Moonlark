7ae2415caffe4772f3124f447ad8af39a2fe5dd2
XiaoDeng3386 <1744793737@qq.com>
2025-09-05 23:18:36 +0800

    记录 GUEST 用户的昵称

--- src/plugins/nonebot_plugin_larkuser/models.py
+++ src/plugins/nonebot_plugin_larkuser/models.py
@@ -8,9 +8,15 @@ from sqlalchemy.orm import Mapped, mapped_column
 class UserData(Model):
     user_id: Mapped[str] = mapped_column(String(128), primary_key=True)
     nickname: Mapped[str] = mapped_column(String(256))
-    register_time: Mapped[Optional[datetime]] = mapped_column(nullable=True, default=None)
+    register_time: Mapped[datetime]
     experience: Mapped[int] = mapped_column(default=0)
     vimcoin: Mapped[float] = mapped_column(default=0.0)
     health: Mapped[float] = mapped_column(default=100.0)
     favorability: Mapped[float] = mapped_column(default=0.0)
     config: Mapped[bytes] = mapped_column(default=b"e30=")
+
+
+class GuestUser(Model):
+    user_id: Mapped[str] = mapped_column(String(128), primary_key=True)
+    nickname: Mapped[str] = mapped_column(String(256))
+
