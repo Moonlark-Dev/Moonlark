8e4acfbe38703e97cd491457e33952c95953264c
XiaoDeng3386 <1744793737@qq.com>
2025-09-06 19:08:00 +0800

    新增广播功能

---
+++ src/plugins/nonebot_plugin_broadcast/__init__.py
@@ -0,0 +1,20 @@
+from nonebot import require
+from nonebot.plugin import PluginMetadata
+
+from .config import Config
+
+__plugin_meta__ = PluginMetadata(
+    name="nonebot-plugin-broadcast",
+    description="Broadcast plugin for sending messages to multiple groups",
+    usage="",
+    config=Config,
+)
+
+require("nonebot_plugin_alconna")
+require("nonebot_plugin_larkutils")
+require("nonebot_plugin_larklang")
+require("nonebot_plugin_orm")
+require("nonebot_plugin_htmlrender")
+require("nonebot_plugin_bots")
+
+from . import __main__
