a91b2e6eb76c98c67b302c3b3477fdf17e95fc29
XiaoDeng3386 <1744793737@qq.com>
2025-09-14 15:50:01 +0800

    add lock in cave add

--- src/plugins/nonebot_plugin_larkcave/plugins/add/__main__.py
+++ src/plugins/nonebot_plugin_larkcave/plugins/add/__main__.py
@@ -1,3 +1,4 @@
+import asyncio
 from datetime import datetime
 from typing import cast
 from nonebot_plugin_alconna import Arparma, Image, Text, UniMessage, image_fetch
@@ -19,6 +20,8 @@ from .encoder import encode_image, encode_text
 from .checker import check_cave
 
 
+lock = asyncio.Lock()
+
 async def get_cave_id(session: async_scoped_session) -> int:
     result = await session.scalar(select(func.max(CaveData.id)))
     return (result + 1) if result is not None else 0
@@ -39,23 +42,24 @@ async def post_cave(
         msg.extend(await decode_cave(e.cave, session, user_id))
         msg.append(Text(await lang.text("add.similarity_footer", user_id, round(e.score * 100, 3))))
         await cave.finish(msg, reply_message=True)
-    cave_id = await get_cave_id(session)
-    content = " ".join(
-        [
-            (
-                (await encode_text(seg.text))
-                if isinstance(seg, Text)
-                else (
-                    await encode_image(
-                        cave_id, seg.name, cast(bytes, await image_fetch(event, bot, state, seg)), session
+    async with lock:
+        cave_id = await get_cave_id(session)
+        content = " ".join(
+            [
+                (
+                    (await encode_text(seg.text))
+                    if isinstance(seg, Text)
+                    else (
+                        await encode_image(
+                            cave_id, seg.name, cast(bytes, await image_fetch(event, bot, state, seg)), session
+                        )
                     )
                 )
-            )
-            for seg in content
-        ]
-    )
-    session.add(CaveData(id=cave_id, author=user_id, time=datetime.now(), content=content))
-    await session.commit()
+                for seg in content
+            ]
+        )
+        session.add(CaveData(id=cave_id, author=user_id, time=datetime.now(), content=content))
+        await session.commit()
     await lang.finish("add.posted", user_id, cave_id)
 
 
