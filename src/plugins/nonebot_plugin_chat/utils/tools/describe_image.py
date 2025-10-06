eb3cdac5de9ee402218a9409439333c8b197da85
XiaoDeng3386 <1744793737@qq.com>
2025-10-06 15:51:35 +0800

    优化图片描述功能，增加URL格式检查和错误提示

--- src/plugins/nonebot_plugin_chat/utils/tools/describe_image.py
+++ src/plugins/nonebot_plugin_chat/utils/tools/describe_image.py
@@ -14,15 +14,27 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # ##############################################################################
-from httpx import AsyncClient
+from httpx import AsyncClient, UnsupportedProtocol
 
 from ..image import request_describe_image
 
 
-async def describe_image(image_url: str) -> str:
+async def _describe_image(image_url: str) -> str:
     async with AsyncClient() as client:
         response = await client.get(image_url)
     if response.is_success:
         image_bytes = response.read()
         return await request_describe_image(image_bytes, "mlsid::--lang=zh-hans")
     return f"获取信息失败 (HTTP {response.status_code})"
+
+
+async def describe_image(image_url: str) -> str:
+    try:
+        return await _describe_image(image_url)
+    except UnsupportedProtocol:
+        if image_url.startswith("来源/梗") or image_url.startswith("[图片"):
+            return (
+                f"传入的 `image_url` 不是合法的 URL，这是从消息中的 `[图片: {image_url}]` 或类似的块中提取出来的文本吗？"
+                f"如果是，请不要再向将这样的内容作为图片 URL 传入，`describe_image` 工具， **`{image_url}` 就是它的描述！**"
+            )
+        return "传入的 `image_url` 不是合法的 URL， **请在使用该工具前检查需要解释的图片 URL 到底是不是完整的URL、有没有指向一个网络图片！**"
