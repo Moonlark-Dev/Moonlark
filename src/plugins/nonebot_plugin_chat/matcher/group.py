0a750a19a26149c8e1109565fbbf4bf3866564bb
XiaoDeng3386 <1744793737@qq.com>
2025-11-22 01:17:55 +0800

    refactor(chat): 调整消息冷却时间设置位置

--- src/plugins/nonebot_plugin_chat/matcher/group.py
+++ src/plugins/nonebot_plugin_chat/matcher/group.py
@@ -371,7 +371,7 @@ class MessageProcessor:
             return
         if (mentioned or not self.session.message_queue) and not self.blocked:
             asyncio.create_task(self.generate_reply(force_reply=mentioned))
-            self.cold_until = datetime.now() + timedelta(seconds=5)
+            
 
     async def handle_group_cold(self, time_d: timedelta) -> None:
         min_str = time_d.total_seconds() // 60
@@ -392,6 +392,7 @@ class MessageProcessor:
             return
         if len(self.openai_messages.messages) <= 0 or not self.openai_messages.is_last_message_from_user():
             return
+        self.cold_until = datetime.now() + timedelta(seconds=5)
 
         # 检查是否应该触发回复
         if not force_reply:
