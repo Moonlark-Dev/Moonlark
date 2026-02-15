eddfe016d555ccf082329337e37a41ec144c1c3a
XiaoDeng3386 <1744793737@qq.com>
2026-02-15 17:38:03 +0800

    feat(epic-freegame): add epic games store free game plugin
    Add `nonebot_plugin_epic_freegame` to project dependencies and packages
    configuration. Include associated localization strings, jinja templates,
    and plugin source files.

---
+++ src/plugins/nonebot_plugin_epic_freegame/__main__.py
@@ -0,0 +1,28 @@
+from nonebot import logger
+from nonebot_plugin_alconna import UniMessage, Alconna, on_alconna
+from nonebot_plugin_larklang import LangHelper
+from nonebot_plugin_larkutils import get_user_id
+from nonebot_plugin_render import render_template
+import httpx
+
+matcher = on_alconna(Alconna("epic-free"), aliases={"epic"})
+lang = LangHelper()
+
+@matcher.handle()
+async def _(user_id: str = get_user_id()) -> None:
+    url = "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=zh-CN&country=CN&allowCountries=CN"
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url)
+        resp.raise_for_status()
+        data = resp.json()
+
+    logger.info("获取 Epic 免费游戏数据成功，正在渲染模版。")
+    await UniMessage().image(
+        raw=await render_template(
+            "epic_free.html.jinja",
+            await lang.text("title", user_id),
+            user_id,
+            {"result": data},
+        )
+    ).send()
+    await matcher.finish()
