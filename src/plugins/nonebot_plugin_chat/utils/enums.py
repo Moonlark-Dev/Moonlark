60deb24ce8162f713b3be51ba73fb40f4e3e9337
XiaoDeng3386 <1744793737@qq.com>
2026-02-23 18:10:50 +0800

    refactor(chat): 优化消息队列处理逻辑

--- src/plugins/nonebot_plugin_chat/utils/enums.py
+++ src/plugins/nonebot_plugin_chat/utils/enums.py
@@ -3,7 +3,5 @@ from enum import Enum, auto
 
 class FetchStatus(Enum):
     SUCCESS = auto()
-    WRONG_TOOL_CALL = auto()
     FAILED = auto()
-    EMPTY_REPLY = auto()
-    NO_MESSAGE_SENT = auto()
+    SKIP = auto()
