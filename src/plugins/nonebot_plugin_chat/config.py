aef5315ab2da3e6fc1d9e9e6539d49606300df22
XiaoDeng3386 <1744793737@qq.com>
2026-04-04 11:51:53 +0800

    refactor(chat): 重构rua reaction配置为全局配置

--- src/plugins/nonebot_plugin_chat/config.py
+++ src/plugins/nonebot_plugin_chat/config.py
@@ -2,6 +2,12 @@ from nonebot import get_plugin_config
 from pydantic import BaseModel
 
 
+class RuaReactionConfig(BaseModel):
+    enjoy: str = "66"
+    dodge: str = "10"
+    bite: str = "128074"
+    pending: str = "181"
+
 class Config(BaseModel):
     """Plugin Config Here"""
 
@@ -12,6 +18,7 @@ class Config(BaseModel):
     vm_api_url: str = ""  # VM 服务地址，如 http://localhost:8000
     vm_api_token: str = ""  # VM API 鉴权 Token
     moonlark_api_base: str = "http://localhost:8080"  # Moonlark API 基础地址
+    rua_reaction_config: RuaReactionConfig = RuaReactionConfig()
 
 
 config = get_plugin_config(Config)
