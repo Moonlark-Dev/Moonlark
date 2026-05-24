1525507663c98ff9437f88cc7cc670c85d3f58dd
This-is-XiaoDeng <1744793737@qq.com>
2026-05-24 16:56:11 +0800

    fix(items): 修复 registry.ITEMS AttributeError
    nonebot_plugin_items/__init__.py 中
    将 registry 绑定为 registry.py 模块，导致 utils 中  找不到。
    
    改为直接从 registry 包导入 ITEMS 和 ResourceLocation。

--- src/plugins/nonebot_plugin_items/utils/get.py
+++ src/plugins/nonebot_plugin_items/utils/get.py
@@ -1,15 +1,15 @@
 from typing import Any
 from ..base.stack import ItemStack
-from .. import registry
+from ..registry import ITEMS, ResourceLocation
 
 
 async def get_item(
-    location: registry.ResourceLocation, user_id: str, count: int = 1, data: dict[str, Any] = {}
+    location: ResourceLocation, user_id: str, count: int = 1, data: dict[str, Any] = {}
 ) -> ItemStack:
     """通过 location 获取物品
 
     Args:
-        location (registry.ResourceLocation): location
+        location (ResourceLocation): location
         user_id (str): 用户 ID（本地化需要）
         count (int, optional): 物品数量. Defaults to 1.
         data (dict[str, Any], optional): 物品NBT. Defaults to {}.
@@ -17,5 +17,5 @@ async def get_item(
     Returns:
         ItemStack: 物品 Stack
     """
-    item = registry.ITEMS.getValue(location)
+    item = ITEMS.getValue(location)
     return ItemStack(item, count, data, user_id)
