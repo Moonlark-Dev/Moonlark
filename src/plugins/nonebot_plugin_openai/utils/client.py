801f4bbdc7088f0a3bb6db026c573f5d9b7005fd
XiaoDeng3386 <1744793737@qq.com>
2026-06-07 12:36:30 +0800

    refactor(openai): remove unused completion utility functions
    Remove deprecated helper functions for handling chat completions to clean up the client utility module.

--- src/plugins/nonebot_plugin_openai/utils/client.py
+++ src/plugins/nonebot_plugin_openai/utils/client.py
@@ -10,22 +10,3 @@ from ..types import Messages
 
 client = AsyncOpenAI(api_key=config.openai_api_key, base_url=config.openai_base_url)
 
-
-async def get_completion(
-    messages: Messages, functions: list[dict[str, Any]], model: str = config.openai_default_model, **kwargs
-) -> ChatCompletion:
-    return await client.chat.completions.create(messages=messages, model=model, **kwargs)
-
-
-def get_reply_message(completion: ChatCompletion) -> ChatCompletionMessage:
-    return completion.choices[0].message
-
-
-def get_reply_content(message: ChatCompletionMessage) -> str:
-    if message.content is None:
-        raise EmptyReplyContent()
-    return message.content
-
-
-def get_reply(completion: ChatCompletion) -> str:
-    return get_reply_content(get_reply_message(completion))
