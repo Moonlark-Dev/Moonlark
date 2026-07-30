d9b670157596ba4f1e3bcb605f1c0389707b63e1
XiaoDeng3386 <1744793737@qq.com>
2026-07-30 12:03:19 +0800

    fix(larkuid): 恢复 patch_header 中的 CORS fallback header

--- src/plugins/nonebot_plugin_larkuid/middlewares.py
+++ src/plugins/nonebot_plugin_larkuid/middlewares.py
@@ -1,12 +1,14 @@
 import time
 from typing import cast
+
 from fastapi import FastAPI, Request, Response
-from nonebot import get_app
 from fastapi.middleware.cors import CORSMiddleware
+from nonebot import get_app
+from starlette.middleware.base import RequestResponseEndpoint
 
 from .config import config
 
-app = cast(FastAPI, get_app())
+app = cast("FastAPI", get_app())
 
 app.add_middleware(
     CORSMiddleware,
@@ -18,9 +20,10 @@ app.add_middleware(
 
 
 @app.middleware("http")
-async def patch_header(request: Request, call_next):
+async def patch_header(request: Request, call_next: RequestResponseEndpoint):
     start_time = time.time()
-    response = cast(Response, await call_next(request))
+    response = cast("Response", await call_next(request))
     process_time = time.time() - start_time
-    response.headers["X-Process-Time"] = f"{round(process_time*1000, 1)} ms"
+    response.headers["X-Process-Time"] = f"{round(process_time * 1000, 1)} ms"
+    response.headers["Access-Control-Allow-Origin"] = config.cors_allow_origins[0] if config.cors_allow_origins else "*"
     return response
